25aa3e1ff6cb5257e582a17f6573839f59b64a94
Jamie Finn <jamiefinn.jf@gmail.com>
2020-08-23 19:47:23 +0100

    first commit

---
+++ resources/assets/scripts/main.js
@@ -0,0 +1,31 @@
+// import external dependencies
+import Vue from 'vue';
+import 'jquery';
+
+// Import everything from autoload
+import './autoload/**/*'
+
+// import local dependencies
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+import aboutUs from './routes/about';
+
+/** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+  // Home page
+  home,
+  // About Us page, note the change from about-us to aboutUs.
+  aboutUs,
+});
+
+// Load Events
+jQuery(document).ready(() => routes.loadEvents());
+
+Vue.component('example', require('./vue/Example.vue').default);
+
+new Vue({
+  el: '#app',
+});
