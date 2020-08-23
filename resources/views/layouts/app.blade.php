68f9a13b0e08c0b96c9d953fc308831449b33814
Jamie Finn <jamiefinn.jf@gmail.com>
2020-08-23 20:23:51 +0100

    Put footer partial view inside Vue root element div

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,9 @@
           @endif
         </div>
       </div>
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
     @php wp_footer() @endphp
   </body>
 </html>
